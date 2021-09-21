a34f4fe6f66e3d184e13afa9554d598cb9588411
duckduckstab1 <84234115+duckduckstab1@users.noreply.github.com>
2021-09-21 16:18:19 +0100

    Benis

---
+++ trashe/more-spaghetti-code.js
@@ -0,0 +1,20 @@
+// actually, this is a needed heavy bullshit thing lol
+var x = 5
+var y = 3
+var sum = x+y
+document.write(sum)
+var My_fuction = new Function("a","b","return a+b")
+var x = My_function(2,3)
+var x = 2
+var y = 3
+var b = 62
+// is x ass?
+var x = My_function(1,3)
+// uerhgiuergiuerhguihergiuhsgirehgheurhguhgiuergiuergejih
+var s = 12
+//
+alert("I want to die.")
+var x = 2
+// is it 2 though?
+var spaghetti? = 99999299292
+document.write('s')
